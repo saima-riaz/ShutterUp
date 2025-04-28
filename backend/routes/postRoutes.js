31861ba049553b90810be7e0262faabe312b1aed
saima-riaz <saimariaz1422@gmail.com>
2025-04-28 19:59:35 +0200

    fix error

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -9,4 +9,7 @@ router.post("/", authMiddleware, postController.createPost);
 // GET route for fetching posts
 router.get("/", authMiddleware, postController.getPosts);
 
+// route to delete photo
+router.delete('/:id', authMiddleware, postController.deletePost);
+
 module.exports = router;
