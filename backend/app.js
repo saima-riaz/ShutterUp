38cb4b8f5c8523ad9e9dd10f157189a3405fb7bb
saima-riaz <saimariaz1422@gmail.com>
2025-05-03 16:07:27 +0200

    added Gallery  route

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ mongoose.connect(process.env.MONGODB_URI)
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/posts", require("./routes/postRoutes"));
+app.use('/api/gallery', require('./routes/galleryRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
