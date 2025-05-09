50e39f750fff0e34ca9cd36214f4f5a3cbf9aded
saima-riaz <saimariaz1422@gmail.com>
2025-05-09 20:20:50 +0200

    modifty and updated

--- backend/routes/galleryRoutes.js
+++ backend/routes/galleryRoutes.js
@@ -3,16 +3,18 @@ const router = express.Router();
 const galleryController = require("../controllers/galleryController");
 const authMiddleware = require("../middleware/authMiddleware");
 
+
 // POST route for creating galleries
 router.post("/create", authMiddleware, galleryController.createGallery);
 
 // GET route for fetching all galleries
 router.get("/", authMiddleware, galleryController.getGalleries);
 
-// GET route for single gallery by URL
+// GET route send image fav gallery
 router.get("/:url", authMiddleware, galleryController.getGalleryByUrl);
 
 // DELETE route for galleries (using _id for MongoDB)
 router.delete('/:_id', authMiddleware, galleryController.deleteGallery);
+
 
 module.exports = router;
