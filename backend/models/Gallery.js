74a0385aabc19ff744414c1dc62fe1066d99468d
saima-riaz <saimariaz1422@gmail.com>
2025-05-11 01:08:59 +0200

    midify  auth verification by JWT token

--- backend/models/Gallery.js
+++ backend/models/Gallery.js
@@ -1,29 +1,31 @@
 const mongoose = require('mongoose');
 
+// Define the schema for the Gallery model
 const gallerySchema = new mongoose.Schema({
   title: {
-  type: String,
-    required: true,
+  type: String, // Title of the gallery
+    required: true, // Title is required
   },
   description: {
-    type: String,
+    type: String, // Description of the gallery
     required: true,
   },
   url: {
-    type: String,
-    required: true,
-    unique: true,
+    type: String, // URL for the gallery
+    required: true, // URL is required
+    unique: true, // // URL should be unique for each gallery
   },
   user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    type: mongoose.Schema.Types.ObjectId, // Reference to the User model
+    ref: 'User', // Reference to the User model to associate a gallery with a user
+    required: true // User is required to associate with the gallery
   },
   photos: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post',
+    type: mongoose.Schema.Types.ObjectId, // Reference to the Post model (photos)
+    ref: 'Post', // Link each photo to a Post
   }],
   
-}, { timestamps: true });
+}, { timestamps: true }); // Enable timestamps for createdAt and updatedAt fields
 
+// Create and export the Gallery model based on the schema
 module.exports = mongoose.model('Gallery', gallerySchema);
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
+router.post('/register', authController.register); // public route
 router.post('/login', authController.login);
 router.get('/verify-email/:token', authController.verifyEmail);
 
