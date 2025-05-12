d6d4a29746b92a1c7a553ea7e6974d4ae7ef72dc
saima-riaz <saimariaz1422@gmail.com>
2025-05-12 15:57:23 +0200

    updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,50 +1,13 @@
-/*module.exports = (req, res, next) => {
-   // Simulate an authenticated user
-  req.user = { 
-    id: "6614a5879f06d5e06d874b1d", // <-- user id of DB
-  };
-  next(); // proceed to next midleware 
-}; */
-
-/*const jwt = require('jsonwebtoken');
-
-// Middleware to protect routes using JWT
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  // Check if token exists and starts with "Bearer"
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'No token provided' });
-  }
-
-  // Extract the token from the header
-  const token = authHeader.split(' ')[1];
-
-  try {
-     // Verify the token using the secret key
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Attach user info to the request
-    req.user = { id: decoded.id };
-
-    // Proceed to the next middleware or route
-    next();
-  } catch (err) {
-    
-    // Token is invalid or expired
-    console.error("JWT verification failed:", err.message);
-    return res.status(401).json({ message: 'Invalid or expired token' });
-  }
-};*/
-
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+//protect routes by verifying JWT & attaching user request
 const authMiddleware = async (req, res, next) => {
   try {
-    // 1. Get token from header
+    // get token from  Authorization header
     const token = req.header('Authorization')?.replace('Bearer ', '');
 
+    // If token is missing, deny access
     if (!token) {
       return res.status(401).json({ message: 'No token, authorization denied' });
     }
@@ -52,15 +15,19 @@ const authMiddleware = async (req, res, next) => {
      // Verify token + check expiry
      const decoded = jwt.verify(token, process.env.JWT_SECRET, { ignoreExpiration: false }); // ✅ Force expiry check
     
-    // 3. Find user and attach to request
+    // find user by id & attach to request
     const user = await User.findById(decoded.id).select('-password');
+    
+    // if user doesn't exist, deny access
     if (!user) {
       return res.status(401).json({ message: 'Invalid token, user not found' });
     }
-
+    //user to request object for access in next routes
     req.user = user;
     next();
   } catch (err) {
+
+    // handle token errors
     console.error('Authentication error:', err.message);
     res.status(401).json({ message: 'Token is not valid' });
   }
