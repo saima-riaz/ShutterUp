e30f6faea5f29ce1d576f68f8509dcd05d924b4f
saima-riaz <saimariaz1422@gmail.com>
2025-08-11 01:17:06 +0200

    add new image

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite' // Import the 'defineConfig' function from Vite
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vite.dev/config/
-// Vite configuration file
 export default defineConfig({
-  plugins: [react()], // Enable the React plugin for Vite
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
+  }
+});
