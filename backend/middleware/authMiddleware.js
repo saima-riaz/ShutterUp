652beb430e553136c759aa05f5bd5b8e3fe727e5
saima-riaz <saimariaz1422@gmail.com>
2025-08-11 16:33:44 +0200

    updated to work upload function

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');  // <-- Make sure this line is present
+const User = require('../models/User');
 
 const authMiddleware = async (req, res, next) => {
   try {
