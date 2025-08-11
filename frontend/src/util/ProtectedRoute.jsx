49ea8848c8081a46c0e51ae6eedab3c298a0255e
saima-riaz <saimariaz1422@gmail.com>
2025-08-11 16:39:04 +0200

    add loading function to stay the user login on refresh page

--- frontend/src/util/ProtectedRoute.jsx
+++ frontend/src/util/ProtectedRoute.jsx
@@ -1,12 +1,15 @@
-import { useAuth } from './AuthContext'; //custom Auth context to access authentication state
+import { useAuth } from './AuthContext';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  const { user } = useAuth(); // Get the current user from Auth context
-  const location = useLocation(); // Get the current location for redirect purposes
+  const { user, loading } = useAuth();
+  const location = useLocation();
 
-  // If the user is authenticated, render the child components (Outlet).
-  // Otherwise, redirect the user to the login page, preserving the current location.
+  if (loading) {
+    // Show loading spinner or nothing while checking auth status
+    return <p>Loading...</p>;
+  }
+
   return user ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />;
 };
 
