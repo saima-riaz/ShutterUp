4a620106cbc233db4fc8663d52e1ac11cd452c23
saima-riaz <saimariaz1422@gmail.com>
2025-08-16 18:48:08 +0200

    updated

--- backend/app.js
+++ backend/app.js
@@ -33,4 +33,4 @@ mongoose.connect(process.env.MONGODB_URI)
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
