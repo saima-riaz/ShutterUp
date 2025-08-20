067cd574cc4cf4f65cd849ea739c3621d4fab87d
saima-riaz <saimariaz1422@gmail.com>
2025-08-20 20:08:18 +0200

    updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,7 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/verify-email/:token', authController.verifyEmail);
 router.post('/refresh-token', authController.refreshToken);
-router.get('/me', authController.getUser);
+//router.get('/me', authController.getUser);
 router.post('/logout', authController.logout);
 
 // Request password reset
