190d94f281eb45be0f2cc1368b606b79f3061289
saima-riaz <saimariaz1422@gmail.com>
2025-09-16 21:13:23 +0200

    updated

--- backend/app.js
+++ backend/app.js
@@ -5,32 +5,41 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
-dotenv.config();
 
+
+dotenv.config();
 const app = express();
 
+// Middleware
 app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',  // frontend URL
-  credentials: true                 // allow cookies
+  origin: "http://localhost:5173", // frontend URL
+  credentials: true,  // allow cookies
+  exposedHeaders: ['set-cookie']
 }));
 
+app.use(fileUpload({
+  useTempFiles: false,
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+  parseNested: true
+}));
+
+// MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(console.error);
 
-  app.use(fileUpload({
-    useTempFiles: false, // not using disk storage
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB max file size
-  }));
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/posts", require("./routes/postRoutes"));
+app.use("/api/gallery", require("./routes/galleryRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/notifications", require("./routes/notificationsRoutes"));
 
-  app.use("/api/auth", require("./routes/authRoutes"));
-  app.use("/api/posts", require("./routes/postRoutes"));
-  app.use("/api/gallery", require("./routes/galleryRoutes"));
-  
-
-
+// Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
