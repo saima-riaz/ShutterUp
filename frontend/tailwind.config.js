03d30f9683f2574536109ced0714ed655503dab0
saima-riaz <saimariaz1422@gmail.com>
2025-12-18 21:30:39 +0100

    update responsiveness view

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,20 +10,20 @@ module.exports = {
     extend: {
       // Extend the default theme with custom values
       
-       fontFamily: {  // Add custom fonts to the theme
-        sans: ['Satisfy", cursive', 'sans-serif'],
+      fontFamily: {
+        sans: ['Satisfy', 'cursive', 'sans-serif'],
         lavish: ['"Lavishly Yours"', 'cursive'],
       },
       
       screens: { // Define custom breakpoints for responsive design
-        'xs': '480px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
+        'xs': '375px',     // Small phones
+        'sm': '640px',     // Phones
+        'md': '768px',     // Tablets
+        'lg': '1024px',    // Laptops
+        'xl': '1280px',    // Desktops
+        '2xl': '1536px',   // Large screens
       }
     },
   },
   plugins: [], // No plugins are included in this setup
-
 }
