f4bf4816fcc38f7d7a47e806c12d5827223d6189
saima-riaz <saimariaz1422@gmail.com>
2026-09-17 22:30:32 +0200

    update  file

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,23 @@
+// Import express library to create routes
+// :Cannot define routes
+const express = require("express");
+const router = express.Router(); // Create router to define routes
+
+// Import user controller for handling profile logic
+// :Routes will have no functionality
+const userController = require("../controllers/userController");
+
+// Import auth middleware to protect routes
+//:Anyone could update user profiles without login
+const authMiddleware = require("../middleware/authMiddleware");
+
+// ======================
+// UPDATE user profile
+// ======================
+// Protected route: user must be logged in
+// :Users cannot update their profile
+router.put("/profile", authMiddleware, userController.updateProfile);
+
+// Export router to use in main app
+// : Routes will not be available in the app
+module.exports = router;
